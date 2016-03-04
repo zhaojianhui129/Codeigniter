bdd92045a36770c10d6f60ea6bfe9b12ed646b9e
zhaojianhui129 <zhaojianhui129@163.com>
2016-03-04 21:18:21 +0800

    整合数据库配置

---
+++ config/database/test.php
@@ -0,0 +1,66 @@
+<?php
+//默认数据库配置
+$db['dflpvmkt']['hostname'] = "127.0.0.1";
+$db['dflpvmkt']['username'] = "greg";
+$db['dflpvmkt']['password'] = "hndxtmx1997";
+$db['dflpvmkt']['database'] = "dflpvmkt";
+$db['dflpvmkt']['dbdriver'] = "mysql";
+$db['dflpvmkt']['dbprefix'] = "";
+$db['dflpvmkt']['pconnect'] = FALSE;//是否使用持续连接（改为否，否则会占用连接资源）
+$db['dflpvmkt']['db_debug'] = TRUE;
+$db['dflpvmkt']['cache_on'] = FALSE;
+$db['dflpvmkt']['cachedir'] = "";
+$db['dflpvmkt']['char_set'] = "latin1";
+$db['dflpvmkt']['dbcollat'] = "latin1_swedish_ci";
+//NTSDF-数据库配置
+$db['ntsdf']['hostname'] = "127.0.0.1";
+$db['ntsdf']['username'] = "greg";
+$db['ntsdf']['password'] = "hndxtmx1997";
+$db['ntsdf']['database'] = "NTSDF";
+$db['ntsdf']['dbdriver'] = "mysql";
+$db['ntsdf']['dbprefix'] = "js_";
+$db['ntsdf']['pconnect'] = FALSE;//是否使用持续连接（改为否，否则会占用连接资源）
+$db['ntsdf']['db_debug'] = TRUE;
+$db['ntsdf']['cache_on'] = FALSE;
+$db['ntsdf']['cachedir'] = "";
+$db['ntsdf']['char_set'] = "utf8";
+$db['ntsdf']['dbcollat'] = "utf8_general_ci";
+//RMP数据库配置
+$db['rmp']['hostname'] = "127.0.0.1";
+$db['rmp']['username'] = "greg";
+$db['rmp']['password'] = "hndxtmx1997";
+$db['rmp']['database'] = "rmp_demo";
+$db['rmp']['dbdriver'] = "mysql";
+$db['rmp']['dbprefix'] = "rmp2016";
+$db['rmp']['pconnect'] = FALSE;//是否使用持续连接（改为否，否则会占用连接资源）
+$db['rmp']['db_debug'] = TRUE;
+$db['rmp']['cache_on'] = FALSE;
+$db['rmp']['cachedir'] = "";
+$db['rmp']['char_set'] = "utf8";
+$db['rmp']['dbcollat'] = "utf8_general_ci";
+//启辰RMP数据库配置
+$db['qirmp']['hostname'] = "127.0.0.1";
+$db['qirmp']['username'] = "greg";
+$db['qirmp']['password'] = "hndxtmx1997";
+$db['qirmp']['database'] = "qirmp_demo";
+$db['qirmp']['dbdriver'] = "mysql";
+$db['qirmp']['dbprefix'] = "qirmp2016";
+$db['qirmp']['pconnect'] = FALSE;//是否使用持续连接（改为否，否则会占用连接资源）
+$db['qirmp']['db_debug'] = TRUE;
+$db['qirmp']['cache_on'] = FALSE;
+$db['qirmp']['cachedir'] = "";
+$db['qirmp']['char_set'] = "utf8";
+$db['qirmp']['dbcollat'] = "utf8_general_ci";
+//执行管控imploment数据库配置
+$db['implement']['hostname'] = "127.0.0.1";
+$db['implement']['username'] = "greg";
+$db['implement']['password'] = "hndxtmx1997";
+$db['implement']['database'] = "implement_demo";
+$db['implement']['dbdriver'] = "mysql";
+$db['implement']['dbprefix'] = "imp2016";
+$db['implement']['pconnect'] = FALSE;//是否使用持续连接（改为否，否则会占用连接资源）
+$db['implement']['db_debug'] = TRUE;
+$db['implement']['cache_on'] = FALSE;
+$db['implement']['cachedir'] = "";
+$db['implement']['char_set'] = "utf8";
+$db['implement']['dbcollat'] = "utf8_general_ci";
---
+++ global.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 全站配置文件
+ * @var unknown
+ */
+
+//网站环境(develop:开发环境,test:测试环境,production)
+if (!defined('ENV')){
+    define('ENV', 'develop');
+}
+
